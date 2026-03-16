07de9fd9e287cafe2b7d4a3f66d2cf47d1175a01
HWIJAE KIM <khjzzang803@gmail.com>
2026-03-17 00:43:42 +0900

    refactor: HitEvents 충돌 처리 로직 개선 및 최근 몬스터 추적 구조 정리 - CollisionLayer가 Ground인 Collider은 저장하지 않도록 변경 - 충돌 Enter 시 Monster의 BodyId와 위치 저장 로직 추가 - 가장 가까운 Monster를 추적하도록 closeId, closePos 관리 구조 개선 - CollisionExit 발생 시 현재 추적 대상과 동일한 BodyId일 경우만 초기화하도록 수정 - 충돌 상태 관리 변수(resetPos, closeId, closePos) 역할 주석 추가

--- MapleStory_Project/Components/HitEvents.cpp
+++ MapleStory_Project/Components/HitEvents.cpp
@@ -22,25 +22,70 @@ void HitEvents::Update()
 //=================================================
 // 충돌 시작 이벤트
 // 다른 Collider와 충돌이 시작될 때 호출된다.
-// 해당 Collider를 currentColliders 목록에 추가하여
-// 현재 충돌 상태를 기록한다.
+// 1. 현재 충돌 중인 Collider 목록에 추가
+// 2. Monster 레이어일 경우
+//    - 가장 가까운 Monster를 추적 대상으로 갱신
+// 가장 가까운 Monster 판단 기준:
+// 플레이어 X 위치 기준 거리 비교
 //=================================================
 void HitEvents::OnCollisionEnter(Collider* other)
 {
 	if (!other) return;
 
+	// Ground Collider은 등록 X
+	if (other->GetCollisionLayer() == CollisionLayer::Ground) return;
+
+	// 현재 충돌 중인 Collider 등록
 	currentColliders.insert(other);	// 충돌 중인 Collider 목록에 추가
+
+	// Monster와 충돌한경움나 처리
+	if (other->GetCollisionLayer() == CollisionLayer::Monster)
+	{
+		auto otherId = other->GetOwner()->GetComponent<RigidBody>("RigidBody")->GetBodyId();
+		b2Vec2 otherPos = b2Body_GetPosition(otherId);
+
+		// 아직 추적 대상이 없다면 바로 등록
+		if (!b2Body_IsValid(nearestMonsterId))
+		{
+			nearestMonsterId = otherId;
+			nearestMonsterPos = otherPos;
+			return;
+		}
+
+		// 기존 대상과 거리 비교 후 더 가까운 Monster로 갱신
+		auto ownerPos = b2Body_GetPosition(GetOwner()->GetComponent<RigidBody>("RigidBody")->GetBodyId());
+
+		if (abs(ownerPos.x - otherPos.x) > abs(ownerPos.x - nearestMonsterPos.x))
+		{
+			nearestMonsterId = otherId;
+			nearestMonsterPos = otherPos;
+		}
+	}
 }
 
 //======================================================================
 // 충돌 종료 이벤트
 // 다른 Collider와의 충돌이 끝날 때 호출된다.
-// 해당 Collider를 currentColliders 목록에서 제거한다.
+// 1. 충돌 목록에서 Collider 제거
+// 2. 종료된 Collider가 현재 추적 대상(closeId)이라면
+//    → 추적 대상 초기화
 //======================================================================
 void HitEvents::OnCollisionExit(Collider* other)
 {
 	if (!other) return;
+
 	currentColliders.erase(other);	// 충돌 목록에서 제거
+
+	// 현재 추적 중이던 Monster가 충돌 종료된 경우
+	if (b2Body_IsValid(nearestMonsterId) &&
+		B2_ID_EQUALS(
+			other->GetOwner()->GetComponent<RigidBody>("RigidBody")->GetBodyId(), 
+			nearestMonsterId))
+	{
+		// 추적 대상 초기화
+		nearestMonsterId = b2_nullBodyId;
+		nearestMonsterPos = kInvalidPosition;
+	}
 }
 
 //======================================================================
